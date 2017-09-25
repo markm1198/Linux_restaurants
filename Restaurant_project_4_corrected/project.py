64ff18e6f25247440ac959e76ef65dcad7ac66cf
markm1198 <mark1198@sbcglobal.net>
2017-09-25 15:20:57 -0700

    copied files from working restaurant

---
+++ Restaurant_project_4_corrected/project.py
@@ -0,0 +1,450 @@
+import json
+import random
+import string
+
+import httplib2
+import requests
+from flask import Flask, render_template, request, \
+    redirect, jsonify, url_for, flash, make_response, \
+    session as login_session
+from oauth2client.client import FlowExchangeError, OAuth2WebServerFlow
+from sqlalchemy import asc
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Base, Restaurant, MenuItem, User, engine
+from variables import facebook_web, google_web, main_app
+
+state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                for x in range(32))
+
+app = Flask(__name__)
+app.secret_key = state
+
+GOOGLE_CLIENT_ID = google_web.get('client_id')
+APPLICATION_NAME = "Restaurant Menu Application"
+
+# Connect to Database and create database session
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+# Create anti-forgery state token
+@app.route('/login')
+def show_login():
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html',
+    STATE=login_session['state'], google_web=google_web)
+
+
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+
+    app_id = facebook_web.get('app_id')
+    app_secret = facebook_web.get('app_secret')
+    url = 'https://graph.facebook.com/oauth/access_token?' \
+          'grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+              app_id, app_secret, str(access_token).split("'")[1])
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    # Use token to get user info from API
+    '''
+        Due to the formatting for the result from the server token exchange we have to
+        split the token first on commas and select the first index which gives us the key : value
+        for the server access token then we split it on colons to pull out the actual token value
+        and replace the remaining quotes with nothing so that it can be used directly in the graph
+        api calls
+    '''
+    str_result = str(result)
+    token = str_result.split(',')[0].split(':')[1].replace('"', '')
+
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout
+    login_session['access_token'] = token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    user_id = get_user_id(login_session['email'])
+    if not user_id:
+        user_id = create_user(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: ' \
+              '150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
+
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    # The access token must me included to successfully logout
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "you have been logged out"
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = OAuth2WebServerFlow(
+            redirect_uri='postmessage',
+            **google_web
+        )
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != GOOGLE_CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+    # ADD PROVIDER TO LOGIN SESSION
+    login_session['provider'] = 'google'
+
+    # see if user exists, if it doesn't make a new one
+    user_id = get_user_id(data["email"])
+    if not user_id:
+        user_id = create_user(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;' \
+              '-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    return output
+
+
+# User Helper Functions
+
+
+def create_user(login_session_dict):
+    new_user = User(name=login_session_dict['username'], email=login_session_dict[
+        'email'], picture=login_session_dict['picture'])
+    session.add(new_user)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session_dict['email']).one()
+    return user.id
+
+
+def get_user_info(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def get_user_id(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
+# DISCONNECT - Revoke a current user's token and reset their login_session
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    # Only disconnect a connected user.
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    if result['status'] == '200':
+        remove_session()
+        response = make_response(json.dumps('Successfully disconnected.'))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.'))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+def remove_session():
+    # the dictionary needs to be copied to avoid a runtime error
+    sess = login_session.copy()
+    for (key, value) in sess.items():
+        del login_session[key]
+
+
+# JSON APIs to view Restaurant Information
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
+def restaurant_menu_json(restaurant_id):
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menu_item_json(restaurant_id, menu_id):
+    menu_item = session.query(MenuItem).filter_by(id=menu_id, restaurant_id=restaurant_id).one()
+    return jsonify(Menu_Item=menu_item.serialize)
+
+
+@app.route('/restaurant/JSON')
+def restaurants_json():
+    restaurants = session.query(Restaurant).all()
+    return jsonify(restaurants=[r.serialize for r in restaurants])
+
+
+# Show all restaurants
+@app.route('/')
+@app.route('/restaurant/')
+def show_restaurants():
+    restaurants = session.query(Restaurant).order_by(asc(Restaurant.name))
+    return render_template('restaurants.html', restaurants=restaurants, login_session=login_session)
+
+
+# Create a new restaurant
+
+
+@app.route('/restaurant/new/', methods=['GET', 'POST'])
+def new_restaurant():
+    if 'username' not in login_session:
+        return redirect('/login')
+    if request.method == 'POST':
+        new_restaurant_obj = Restaurant(
+            name=request.form['name'], user_id=login_session['user_id'])
+        session.add(new_restaurant_obj)
+        flash('New Restaurant %s Successfully Created' % new_restaurant_obj.name)
+        session.commit()
+        return redirect(url_for('show_restaurants'))
+    else:
+        return render_template('newRestaurant.html')
+
+
+# Edit a restaurant
+
+
+@app.route('/restaurant/<int:restaurant_id>/edit/', methods=['GET', 'POST'])
+def edit_restaurant(restaurant_id):
+    edited_restaurant = session.query(
+        Restaurant).filter_by(id=restaurant_id).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if edited_restaurant.user_id != login_session['user_id']:
+        return "<script>function myFunction() {" \
+               "alert('You are not authorized to edit this restaurant. " \
+               "Please create your own restaurant in order to edit.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        if request.form['name'] != edited_restaurant.name:
+            edited_restaurant.name = request.form['name']
+            flash('Restaurant Successfully Edited %s' % edited_restaurant.name)
+        return redirect(url_for('show_restaurants'))
+    else:
+        return render_template('editRestaurant.html', restaurant=edited_restaurant)
+
+
+# Delete a restaurant
+@app.route('/restaurant/<int:restaurant_id>/delete/', methods=['GET', 'POST'])
+def delete_restaurant(restaurant_id):
+    restaurant_to_delete = session.query(
+        Restaurant).filter_by(id=restaurant_id).one()
+    if 'username' not in login_session:
+        return redirect('/login')
+    if restaurant_to_delete.user_id != login_session['user_id']:
+        return "<script>function myFunction() {" \
+               "alert('You are not authorized to delete this restaurant. " \
+               "Please create your own restaurant in order to delete.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        session.delete(restaurant_to_delete)
+        flash('%s Successfully Deleted' % restaurant_to_delete.name)
+        session.commit()
+        return redirect(url_for('show_restaurants', restaurant_id=restaurant_id))
+    else:
+        return render_template('deleteRestaurant.html', restaurant=restaurant_to_delete)
+
+
+# Show a restaurant menu
+
+
+@app.route('/restaurant/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/menu/')
+def show_menu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    creator = get_user_info(restaurant.user_id)
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return render_template('menu.html', items=items, restaurant=restaurant, creator=creator,
+                           login_session=login_session)
+
+
+# Create a new menu item
+@app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
+def new_menu_item(restaurant_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    if login_session['user_id'] != restaurant.user_id:
+        return "<script>function myFunction() {" \
+               "alert('You are not authorized to add menu items to this restaurant. " \
+               "Please create your own restaurant in order to add items.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        new_item = MenuItem(name=request.form['name'], description=request.form['description'], price=request.form[
+            'price'], course=request.form['course'], restaurant_id=restaurant_id, user_id=restaurant.user_id)
+        session.add(new_item)
+        session.commit()
+        flash('New Menu %s Item Successfully Created' % new_item.name)
+        return redirect(url_for('show_menu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+
+
+# Edit a menu item
+
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit', methods=['GET', 'POST'])
+def edit_menu_item(restaurant_id, menu_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    edited_item = session.query(MenuItem).filter_by(id=menu_id).one()
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    if login_session['user_id'] != restaurant.user_id:
+        return "<script>function myFunction() {" \
+               "alert('You are not authorized to edit menu items to this restaurant. " \
+               "Please create your own restaurant in order to edit items.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        for (key, value) in request.form.items():
+            setattr(edited_item, key, value)
+        session.add(edited_item)
+        session.commit()
+        flash('Menu Item Successfully Edited')
+        return redirect(url_for('show_menu', restaurant_id=restaurant_id))
+    else:
+        return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=edited_item)
+
+
+# Delete a menu item
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete', methods=['GET', 'POST'])
+def delete_menu_item(restaurant_id, menu_id):
+    if 'username' not in login_session:
+        return redirect('/login')
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    item_to_delete = session.query(MenuItem).filter_by(id=menu_id).one()
+    if login_session['user_id'] != restaurant.user_id:
+        return "<script>function myFunction() {" \
+               "alert('You are not authorized to delete menu items to this restaurant. " \
+               "Please create your own restaurant in order to delete items.');}</script><body onload='myFunction()''>"
+    if request.method == 'POST':
+        session.delete(item_to_delete)
+        session.commit()
+        flash('Menu Item Successfully Deleted')
+        return redirect(url_for('show_menu', restaurant_id=restaurant_id))
+    else:
+        return render_template('deleteMenuItem.html', item=item_to_delete)
+
+
+# Disconnect based on provider
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        remove_session()
+        flash("You have successfully been logged out.")
+        return redirect(url_for('show_restaurants'))
+    else:
+        flash("You were not logged in")
+        return redirect(url_for('show_restaurants'))
+
+
+if __name__ == '__main__':
+    app.secret_key = main_app.get('secret_key', 'super_secret_key')
+    app.debug = main_app.get('debug', True)
+    app.config['SESSION_TYPE'] = main_app.get('session_type', 'filesystem')
+    app.run(
+        host=main_app.get('host', '0.0.0.0'),
+        port=main_app.get('port', 5000)
+    )
---
+++ Restaurant_project_4_corrected/readme.md
@@ -0,0 +1,17 @@
+﻿README.txt
+ This is project 4 in Udacity's Full Stack Web Developers Nano degree program. This project utilizes CRUD (Create, Read, Update and Delete) and Oauth2 in a python environment.
+  By building a web site that uses Google sign in authorization and an interactive CRUD environment, authorized users can update and delete Restaurants and Menu items.
+
+
+
+To run this project from your terminal     $ cd project_4
+Initiate a virtual python environment      $ virtualenv venv
+Start the virtual environment     		     $ source venv/bin/activate
+Install the necessary dependencies         $ pip install -r requirements.txt
+Load the database			                     $ python lotsofmenus.py
+to run application				                 $ python project.py
+
+
+To view the site visit HTTP://localhost:5000
+
+This site relies heavily on the 'Original UD330 Github repository' and the 'NoMan2000/ud330 fix errors and bad practices repository'.
